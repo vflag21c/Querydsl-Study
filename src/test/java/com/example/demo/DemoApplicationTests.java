bd329a5631c836d33251007b4962af25ebb0f3ad
YouYeol <vflag21c@nate.com>
2024-03-20 23:48:51 +0900

    QueryDsl 학습 1일차

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import com.example.demo.entity.Hello;
+import com.example.demo.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@Transactional
+class DemoApplicationTests {
+
+    @Autowired
+//    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+    }
+
+}
